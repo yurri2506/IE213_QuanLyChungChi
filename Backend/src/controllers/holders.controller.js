c82f3f56bc4364f4050b232b0324b27a60548ad9
Le Ngoc Lan <lengoclan2501@gmail.com>
2025-04-21 14:10:06 +0700

    api holder

--- Backend/src/controllers/holders.controller.js
+++ Backend/src/controllers/holders.controller.js
@@ -1,4 +1,4 @@
-const holderService = require("../services/holders.service")
+const holderService = require("../services/holders.service");
 
 // Controller lấy thông tin Holder
 const getHolderProfileController = async (req, res) => {
@@ -17,6 +17,55 @@ const getHolderProfileController = async (req, res) => {
   }
 };
 
+const getDegreesController = async (req, res) => {
+  try {
+    const { sub } = req.user; // holder_id từ JWT
+    const holder = await holderService.getHolderProfile(sub);
+    const degrees = await holderService.getDegreesByHolder(holder.DID);
+    res.status(200).json({
+      message: "Degrees retrieved successfully",
+      data: degrees,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+      status: "ERROR",
+    });
+  }
+};
+
+/**
+ * * Gửi ZKP + tuyên bố đến verifier
+ */
+// const sendProofToVerifierController = async (req, res) => {
+//   try {
+//     const { verifierDID } = req.params;
+//     const { proof, statement } = req.body;
+//     const { sub } = req.user;
+
+//     if (!proof || !statement) {
+//       return res.status(400).json({ message: "Missing proof or statement" });
+//     }
+
+//     const holder = await holderService.getHolderProfile(sub);
+
+//     const result = await holderService.sendProofToVerifier(verifierDID, {
+//       proof,
+//       statement,
+//       holder_did: holder.holder_did,
+//     });
+
+//     res.status(200).json(result);
+//   } catch (error) {
+//     res.status(error.message === "Verifier not found" ? 404 : 500).json({
+//       message: error.message,
+//       status: "ERROR",
+//     });
+//   }
+// };
+
 module.exports = {
   getHolderProfileController,
+  getDegreesController,
+  //sendProofToVerifierController,
 };
--- Backend/src/routes/holders.route.js
+++ Backend/src/routes/holders.route.js
@@ -4,7 +4,20 @@ const router = express.Router();
 const holderController = require("../controllers/holders.controller");
 const { holderMiddleware } = require("../middleware/auth.middleware");
 
-router.get("/get-details", holderMiddleware,  holderController.getHolderProfileController);
+router.get(
+  "/get-details",
+  holderMiddleware,
+  holderController.getHolderProfileController
+);
 
+// Lấy các bằng cấp đã nhận
+router.get("/degrees", holderMiddleware, holderController.getDegreesController);
+
+// Gửi ZKP và tuyên bố đến verifier
+// router.post(
+//   "/verifiers/proofs/:verifierDID",
+//   holderMiddleware,
+//   holderController.sendProofToVerifierController
+// );
 
 module.exports = router;
